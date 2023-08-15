2379abf78deb37301f37071f892b62fa87753ed3
bwilkins96 <ben.jr.speed@gmail.com>
2023-08-15 19:14:12 -0400

    main requirements

--- week_7.js
+++ week_7.js
@@ -1,8 +1,30 @@
+// SWDV 640: User Interface Design
+// Week 7 JavaScript Assignment
+
+class Concert {
+    constructor(title, venue, time, date, min, max, image='images/wireframe_img.png') {
+        this.title = title;
+        this.venue = venue;
+        this.time = time;
+        this.date = date;
+        this.price = { min: min, max: max }
+        this.image = image;
+    }
+}
+
 function main() {
-    const template = $("#concertTarget").html();
+    const concerts = [
+        new Concert('Concert', 'Venue', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00'),
+        new Concert('Concert', 'Venue', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00'),
+        new Concert('Concert', 'Venue', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00'),
+        new Concert('Concert', 'Venue', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00')
+    ]
+
+    const template = $("#concertTemplate").html();
     Mustache.parse(template);
 
-    console.log(Mustache);
+    const rendered = Mustache.render(template, { concerts: concerts });
+    $("#concertTarget").html(rendered);
 }
 
 $(document).ready(function() {
