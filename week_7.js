b7432955832ffb8f1abb48a99302a1a7c02e6d58
bwilkins96 <ben.jr.speed@gmail.com>
2023-08-16 14:42:24 -0400

    added images and names

--- week_7.js
+++ week_7.js
@@ -2,23 +2,23 @@
 // Week 7 JavaScript Assignment
 
 class Concert {
-    constructor(title, venue, time, date, min, max, image='images/wireframe_img.png') {
+    constructor(title, venue, time, date, min, max, image='wireframe_img.png') {
         this.title = title;
         this.venue = venue;
         this.time = time;
         this.date = date;
-        this.price = { min: min, max: max }
-        this.image = image;
+        this.price = { min: min, max: max };
+        this.image = 'images/' + image;
     }
 }
 
 function main() {
     const concerts = [
-        new Concert('Concert', 'Venue', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00'),
-        new Concert('Concert', 'Venue', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00'),
-        new Concert('Concert', 'Venue', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00'),
-        new Concert('Concert', 'Venue', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00')
-    ]
+        new Concert("Rock N' Roll Tour", 'Outdoor Pavilion', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00', 'rock.jpg'),
+        new Concert('Pop Singer', 'Stadium', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00', 'pop.jpg'),
+        new Concert('Country Band', 'State Fair', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00', 'country.jpg'),
+        new Concert('Symphony', 'Local Theatre', '8:00 PM', 'dd/mm/yyyy', '25.00', '500.00', 'classical.jpg')
+    ];
 
     const template = $("#concertTemplate").html();
     Mustache.parse(template);
